aea953aa616d4c9d28c6479cf5ea96f9ef216925
xpuxo <82038300+xpuxo@users.noreply.github.com>
2021-04-06 23:42:59 +0900

    Nnm 反応bot

--- discordbot.py
+++ discordbot.py
@@ -6,16 +6,16 @@ bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
+import discord
 
+client = discord.Client()
 
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
+@client.event
+async def on_reaction_add(reaction, user):
+    # author: リアクションがついたメッセージを書いた人
+    author = reaction.message.author
+    await client.send_message(author, f"{user} さんがリアクションをしました")
+
 
 
 bot.run(token)
