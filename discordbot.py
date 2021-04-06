4f550bda802070a995426879fdc3021b1a228705
xpuxo <82038300+xpuxo@users.noreply.github.com>
2021-04-06 23:45:05 +0900

    Nnm

--- discordbot.py
+++ discordbot.py
@@ -1,21 +1,15 @@
-from discord.ext import commands
-import os
-import traceback
+import discord
 
+client = discord.Client()
 bot = commands.Bot(command_prefix='/')
 token = os.environ['DISCORD_BOT_TOKEN']
 
+@client.event
+async def on_reaction_add(reaction, user):
+    # author: リアクションがついたメッセージを書いた人
+    author = reaction.message.author
+    await client.send_message(author, f"{user} さんがリアクションをしました")
 
-@bot.event
-async def on_command_error(ctx, error):
-    orig_error = getattr(error, "original", error)
-    error_msg = ''.join(traceback.TracebackException.from_exception(orig_error).format())
-    await ctx.send(error_msg)
-
-
-@bot.command()
-async def ping(ctx):
-    await ctx.send('pong')
 
 
 bot.run(token)
